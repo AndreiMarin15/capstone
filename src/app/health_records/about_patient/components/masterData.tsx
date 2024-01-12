731a5cb15b6051ffbcde37c4d98201215ccefe32
Andrei <misael_marin@dlsu.edu.ph>
2024-01-13 01:19:58 +0800

    update

---
+++ src/app/health_records/about_patient/components/masterData.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+export default function MasterData() {
+	const mData = [
+		{
+			src: "https://cdn.builder.io/api/v1/image/assets/TEMP/86bc0813aecf897cafa42df901705c229a0a744cbf822394277aece4f7f5aa61?",
+			variable: "Name",
+			value: "Juan Dela Cruz",
+		},
+		{
+			src: "https://cdn.builder.io/api/v1/image/assets/TEMP/bdc83ab0b012624934a85572bc069777ad324e289e4cc66764a07f718b44bf9d?",
+			variable: "Age",
+			value: "74",
+		},
+		{
+			src: "https://cdn.builder.io/api/v1/image/assets/TEMP/0d5b3fd16181b4dc9f9076e56dab03643403ad4fe1376a451f5d70c8bc0fcd95?",
+			variable: "Birthday",
+			value: "January 01, 1950",
+		},
+		{
+			src: "https://cdn.builder.io/api/v1/image/assets/TEMP/4c3ec2f045c5a91d05c1f074f660097897b8fc83403da81ed7f44111303ef22f?",
+			variable: "Gender",
+			value: "Male",
+		},
+		{
+			src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/e77ec5f69c4c6a607193ae426085edd6fc84819ef906d2d9ebb491b796c8519b?"',
+			variable: "Address",
+			value: "1 Pasay Rd. Pasay City, Metro Manila",
+		},
+		{
+			src: "https://cdn.builder.io/api/v1/image/assets/TEMP/d354e02d857f0929bd9b58b2f172642a26d8df38bfdf167b22bd115bfe9b4fea?",
+			variable: "Allergies",
+			value: "Penicillin",
+		},
+	];
+	return (
+		<>
+			<div className="text-black text-xs font-bold leading-5 mt-8 mb-8 max-md:ml-1 max-md:mt-10">MASTER DATA</div>
+
+			<table className="max-w-[750px] border-spacing-y-7 border-separate">
+				{mData.map((item) => (
+					<tr key={item.variable}>
+						<td className="w-5">
+							<Image alt="picture" height={0} width={0} loading="lazy" src={item.src} className="w-5" />
+						</td>
+						<td className="border-l-[16px] border-transparent">
+							<div className="text-black text-xs font-semibold leading-5 self-center my-auto">{item.variable}</div>
+						</td>
+						<td className="border-l-[16px] border-transparent">
+							<div className="text-black text-xs leading-5 ml-auto">{item.value}</div>
+						</td>
+					</tr>
+				))}
+			</table>
+		</>
+	);
+}
--- src/app/health_records/about_patient/page.tsx
+++ src/app/health_records/about_patient/page.tsx
@@ -3,56 +3,44 @@ import * as React from "react";
 import Navbar from "../../navbar";
 import HealthRecordsNav from "../healthRecordsNav";
 import PatientProfile from "../patientProfile";
-import MedicalHistory from "./medicalHistory";
-import MasterData from "./masterData";
+import MedicalHistory from "./components/medicalHistory";
+import MasterData from "./components/masterData";
 import { useHRNav } from "@/app/store";
+import FamilyHistory from "./components/familyHistory";
 
 export default function MyComponent() {
-	const {selected} = useHRNav();
+	const { selected } = useHRNav();
 
 	return (
-		<div className="border bg-white flex flex-col items-stretch border-solid border-stone-300">
-			<div className="w-full max-md:max-w-full">
-				<div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-					<div className="flex flex-col items-stretch w-[17%] max-md:w-full max-md:ml-0">
-						<Navbar />
-					</div>
+		<>
+			<div className="border bg-white flex flex-col items-stretch border-solid border-stone-300">
+				<div className="w-full max-md:max-w-full">
+					<div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+						<div className="flex flex-col items-stretch w-[17%] max-md:w-full max-md:ml-0">
+							<Navbar />
+						</div>
 
-					<div className="flex flex-col items-stretch w-[83%] ml-5 max-md:w-full max-md:ml-0 h-[100vh]">
-						<span className="flex flex-col mt-8 px-5 max-md:max-w-full max-md:mt-10">
-							<span className="flex w-[221px] max-w-full flex-col items-stretch self-start">
-								<div className="text-black text-xl font-semibold leading-8">Health Records</div>
-								<PatientProfile />
-								{/* <div className="flex items-stretch justify-between gap-3.5 mt-10">
-									<img
-										loading="lazy"
-										src="https://cdn.builder.io/api/v1/image/assets/TEMP/f93d5b041a77641729755adbc288033a6c368ab9f2f47627fb102ac12928179c?"
-										className="aspect-square object-contain object-center w-[59px] overflow-hidden shrink-0 max-w-full"
-									/>
-									<span className="flex grow basis-[0%] flex-col items-stretch mt-4 self-start">
-										<div className="text-black text-xs font-semibold leading-5">DELA CRUZ, Juan</div>
-										<div className="flex items-center">
-											<div className="text-black text-xs leading-5 whitespace-nowrap pr-2">74 years old</div>
-											<div className="relative">
-												<div className="bg-stone-300 w-px h-4 top-0 bottom-0 left-1/2 transform -translate-x-1/2" />
-											</div>
-											<div className="text-black text-xs leading-5 whitespace-nowrap pl-2">Male</div>
-										</div>
-									</span>
-								</div> */}
+						<div className="flex flex-col items-stretch w-[83%] ml-5 max-md:w-full max-md:ml-0 h-[100vh]">
+							<span className="flex flex-col mt-8 px-5 max-md:max-w-full max-md:mt-10">
+								<span className="flex w-[221px] max-w-full flex-col items-stretch self-start">
+									<div className="text-black text-xl font-semibold leading-8">Health Records</div>
+									<PatientProfile />
+								</span>
+								<HealthRecordsNav />
+								{selected === "Master Data" ? (
+									<MasterData />
+								) : selected === "Medical History" ? (
+									<MedicalHistory />
+								) : selected === "Family History" ? (
+									<FamilyHistory />
+								) : (
+									""
+								)}
 							</span>
-							<HealthRecordsNav />
-							{
-								selected === "Master Data" ? <MasterData /> :
-								selected === "Medical History" ? <MedicalHistory /> :
-								""
-							}
-							
-							
-						</span>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
