2aed2c0c1a1fe08506ad3017aacaed789bae3331
Andrei <misael_marin@dlsu.edu.ph>
2024-01-13 19:46:15 +0800

    finalized

--- src/app/patient_form/social_history/page.tsx
+++ src/app/patient_form/social_history/page.tsx
@@ -10,31 +10,7 @@ import Image from "next/image";
 export default function family_history() {
   return (
     <div className="border bg-white flex flex-col items-stretch pb-8 border-solid border-stone-300">
-      <div className="shadow-sm bg-white flex w-full flex-col justify-center pl-4 pr-16 py-3 items-start max-md:max-w-full max-md:pr-5 max-sm:hidden">
-        <span className="flex items-stretch gap-2">
-          <div className="flex-col fill-[radial-gradient(59.93%_59.93%_at_50%_50%,#D9D9D9_0%,#3B82F6_0.01%,#A4CFFF_45.83%,#00358C_100%)] overflow-hidden relative flex aspect-square w-[22px] items-center pt-2.5">
-            <Image 
-            alt="picture"
-            height={0}
-            width={0}
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c3f4d00fff88c11c0b749d752a37d84fc9a7f14af72ad4d2717c52156cc88e1?"
-              className="absolute h-full w-full object-cover object-center inset-0"
-            />
-            <Image 
-            alt="picture"
-            height={0}
-            width={0}
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/52c2481ba9b5fc07f4dfb5bb7e3e915846d4b1f7f4bc690044aa72edf8a759a5?"
-              className="aspect-[1.69] object-contain object-center w-full fill-[radial-gradient(50.07%_88.54%_at_50.07%_11.46%,#3B82F6_0%,rgba(30,64,175,0.00)_100%)] overflow-hidden"
-            />
-          </div>
-          <div className="text-blue-500 text-base font-bold leading-6 self-center grow whitespace-nowrap my-auto">
-            EndoTracker
-          </div>
-        </span>
-      </div>
+      
       <span className="flex w-full flex-col mt-11 px-20 max-md:max-w-full max-md:mt-10 max-md:px-5">
         <span className="self-stretch flex items-center justify-between gap-5 ml-4 mr-4 max-md:max-w-full max-md:flex-wrap max-md:mr-2.5">
           <div className="text-black text-xl font-semibold leading-8 my-auto">
