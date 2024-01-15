2f4fa6bd89d44ef451a374d4fa6555e77a59ba5b
Andrei <misael_marin@dlsu.edu.ph>
2024-01-15 21:35:54 +0800

    update

--- src/app/patient_form/page.tsx
+++ src/app/patient_form/page.tsx
@@ -39,30 +39,37 @@ export default function PatientInformation() {
 				</span>
 
 				<ProgressBar currentStep={currentState} />
-
-				{currentState === 1 ? (
-					<SignUpPersonalInformation />
-				) : currentState === 2 ? (
-					<SignUpFamilyHistory />
-				) : currentState === 3 ? (
-					<SignUpSocialHistory />
-				) : currentState === 4 ? (
-					<SignUpMedicalHistory />
-				) : (
-					<SignUpPersonalInformation />
-				)}
+				<>
+				<div className={currentState === 4 ? "pb-20" : ''}>
+					{currentState === 1 ? (
+						<SignUpPersonalInformation />
+					) : currentState === 2 ? (
+						<SignUpFamilyHistory />
+					) : currentState === 3 ? (
+						<SignUpSocialHistory />
+					) : currentState === 4 ? (
+						<SignUpMedicalHistory />
+					) : (
+						<SignUpPersonalInformation />
+					)}
+				</div>
+				</>
 
 				<div className="w-full flex justify-between px-14 max-md:max-w-full  max-md:px-5">
-					<button
-						onClick={() => {
-							if (currentState > 1) {
-								setCurrentState((currentState - 1) as 1 | 2 | 3 | 4);
-							}
-						}}
-						className="mt-5 text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-gray-400 self-stretch mr-2  px-6 py-2 rounded max-md:px-5"
-					>
-						BACK
-					</button>
+					{currentState > 1 ? (
+						<button
+							onClick={() => {
+								if (currentState > 1) {
+									setCurrentState((currentState - 1) as 1 | 2 | 3 | 4);
+								}
+							}}
+							className="mt-5 text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-gray-400 self-stretch mr-2  px-6 py-2 rounded max-md:px-5"
+						>
+							BACK
+						</button>
+					) : (
+						<div></div>
+					)}
 					<button
 						onClick={() => {
 							if (currentState < 4) {
