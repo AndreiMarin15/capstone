c47da3613c9ea607bf60cc4b215e9528621b6ea2
yanajuson <kay_eliana_juson@dlsu.edu.ph>
2024-01-17 01:22:40 +0800

    labtest list and updated margins

--- src/app/viewing/view_careplanlist/page.tsx
+++ src/app/viewing/view_careplanlist/page.tsx
@@ -15,7 +15,7 @@ export default function ViewCarePlanList() {
 
 	return (
 		<div className="border h-full w-full bg-white flex flex-col items-center px-20 py-12 border-solid border-stone-300 max-md:px-5">
-			<div className="flex w-full max-w-[928px] items-stretch justify-between gap-5 mt-11 max-md:max-w-full max-md:flex-wrap max-md:mt-10">
+			<div className="flex w-full items-stretch justify-between gap-5 mt-11 max-md:max-w-full max-md:flex-wrap max-md:mt-10">
 				<span className="flex flex-col items-stretch">
 					<div className="text-black text-xl font-semibold leading-8 whitespace-nowrap">
 						Care Plans and Laboratory Tests
@@ -30,11 +30,11 @@ export default function ViewCarePlanList() {
 							width={0}
 							loading="lazy"
 							src="https://cdn.builder.io/api/v1/image/assets/TEMP/e2aee5eaae6c8b317fa94c9456603d2ba5c59247e65984390a06ee8f8b01312c?"
-							className="aspect-square object-contain object-center w-[13px] fill-stone-300 overflow-hidden shrink-0 max-w-full"
+							className=" ml-2 aspect-square object-contain object-center w-[13px] fill-stone-300 overflow-hidden shrink-0 max-w-full"
 						/>
 						<input className="text-stone-300 text-xs leading-5 my-auto" placeholder="SEARCH"></input>
 					</span>
-					<span className="flex items-stretch justify-between gap-1 px-2.5 py-2 rounded-md border-[0.5px] border-solid border-black">
+					<button className="flex items-stretch justify-between gap-1 px-2.5 py-2 rounded-md border-[0.5px] border-solid border-black">
 						<Image
 							alt="picture"
 							height={0}
@@ -44,7 +44,7 @@ export default function ViewCarePlanList() {
 							className="aspect-[0.86] object-contain object-center w-3 overflow-hidden shrink-0 max-w-full"
 						/>
 						<div className="text-black text-xs leading-5 self-center grow whitespace-nowrap my-auto">FILTER</div>
-					</span>
+					</button>
 				</div>
 			</div>
 			{/* TODO: UPDATE IMAGE SRC */}
