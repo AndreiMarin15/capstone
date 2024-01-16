3fe121a5f1a64fa8cc4ee0a3bf4b47e9e704a4b0
Andrei <misael_marin@dlsu.edu.ph>
2024-01-17 00:13:02 +0800

    care_plan_mapping

--- src/app/viewing/view_careplan/page.tsx
+++ src/app/viewing/view_careplan/page.tsx
@@ -2,8 +2,49 @@ import Image from "next/image";
 import * as React from "react";
 
 export default function viewCarePlan() {
+	const cplan = [
+		{
+			variable: "Brand Name",
+			imgsrc:
+				"https://cdn.builder.io/api/v1/image/assets/TEMP/c30a6e4fc539ac988d28f6ab6785ceeca11c1aaa77f6d0272dd9cc6a0a0d3820?apiKey=66e07193974a40e683930e95115a1cfd&",
+			value: ["Losartan"],
+		},
+		{
+			variable: "Generic Name",
+			imgsrc:
+				"https://cdn.builder.io/api/v1/image/assets/TEMP/c30a6e4fc539ac988d28f6ab6785ceeca11c1aaa77f6d0272dd9cc6a0a0d3820?apiKey=66e07193974a40e683930e95115a1cfd&",
+			value: ["Losartan"],
+		},
+		{
+			variable: "Dose",
+			imgsrc:
+				"https://cdn.builder.io/api/v1/image/assets/TEMP/4ed9fdfc8e5d8d154f4e19144450c46befc8c837076372eb4558e3c022a2dd62?apiKey=66e07193974a40e683930e95115a1cfd&",
+			value: ["50mg"],
+		},
+		{
+			variable: "Form",
+			imgsrc:
+				"https://cdn.builder.io/api/v1/image/assets/TEMP/87ca70ae252b347ccd1a7f94322c75fde5eccbeaecf0c288f0c1483be1e110ce?apiKey=66e07193974a40e683930e95115a1cfd&",
+			value: ["Tablet"],
+		},
+		{
+			variable: "Quantity",
+			imgsrc:
+				"https://cdn.builder.io/api/v1/image/assets/TEMP/0cab09a72c90afc03696d3dc2ca45a2e63e8b730e21455af9a7bf1772fd796a5?apiKey=66e07193974a40e683930e95115a1cfd&",
+			value: ["30"],
+		},
+		{
+			variable: "Note",
+			imgsrc:
+				"https://cdn.builder.io/api/v1/image/assets/TEMP/d4f42d9fd339eec4a1dd4cc1ab3febb85775d21fcc445ac09b38ef2ea22866d1?apiKey=66e07193974a40e683930e95115a1cfd&",
+			value: [
+				"1 tablet 1x/ day for 30 days",
+				"30 minutes of moderate-intensity exercise (e.g., brisk walking, swimming, or cycling) at least five days a week.",
+			],
+		},
+	];
 	return (
-		<div className="h-full border bg-white flex flex-col px-20 py-12 border-solid border-stone-300 max-md:px-5">
+		<div className="h-[100vh] border bg-white flex flex-col px-20 py-12 border-solid border-stone-300 max-md:px-5 w-full">
 			<span className="self-start flex w-full max-w-[918px] items-center justify-between gap-5 mt-12 px-px max-md:max-w-full max-md:flex-wrap max-md:mt-10">
 				<div className="text-black text-xl font-semibold leading-8 flex-1 my-auto">Care Plan #1</div>
 				<span className="self-stretch flex items-stretch justify-between gap-2">
@@ -47,74 +88,25 @@ export default function viewCarePlan() {
 				</div>
 			</span>
 
-			<div className="self-start w-full max-w-[918px] mt-10 px-2 max-md:max-w-full">
-				<div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-					<div className="flex flex-col items-stretch w-[21%] max-md:w-full max-md:ml-0">
-						<div className="flex items-center gap-4 my-auto max-md:mt-10">
-							<div className="flex basis-[0%] flex-col items-center my-auto">
-								<Image
-									alt="picture"
-									height={0}
-									width={0}
-									loading="lazy"
-									src="https://cdn.builder.io/api/v1/image/assets/TEMP/c30a6e4fc539ac988d28f6ab6785ceeca11c1aaa77f6d0272dd9cc6a0a0d3820?"
-									className="aspect-square object-contain object-center w-6 overflow-hidden"
-								/>
-								<Image
-									alt="picture"
-									height={0}
-									width={0}
-									loading="lazy"
-									src="https://cdn.builder.io/api/v1/image/assets/TEMP/4ed9fdfc8e5d8d154f4e19144450c46befc8c837076372eb4558e3c022a2dd62?"
-									className="aspect-[2.5] object-contain object-center w-6 overflow-hidden mt-8"
-								/>
-								<Image
-									alt="picture"
-									height={0}
-									width={0}
-									loading="lazy"
-									src="https://cdn.builder.io/api/v1/image/assets/TEMP/87ca70ae252b347ccd1a7f94322c75fde5eccbeaecf0c288f0c1483be1e110ce?"
-									className="aspect-[1.41] object-contain object-center w-6 overflow-hidden mt-10"
-								/>
-								<Image
-									alt="picture"
-									height={0}
-									width={0}
-									loading="lazy"
-									src="https://cdn.builder.io/api/v1/image/assets/TEMP/0cab09a72c90afc03696d3dc2ca45a2e63e8b730e21455af9a7bf1772fd796a5?"
-									className="aspect-[1.2] object-contain object-center w-6 overflow-hidden mt-8"
-								/>
-							</div>
-							<span className="items-stretch self-stretch flex grow basis-[0%] flex-col">
-								<div className="text-black text-xs font-semibold leading-5 whitespace-nowrap">Brand Name</div>
-								<div className="text-black text-xs font-semibold leading-5 whitespace-nowrap mt-9">Generic Name</div>
-								<div className="text-black text-xs font-semibold leading-5 mt-9">Dose</div>
-								<div className="text-black text-xs font-semibold leading-5 mt-9">Form</div>
-								<div className="text-black text-xs font-semibold leading-5 mt-9">Quantity</div>
-								<div className="text-black text-xs font-semibold leading-5 mt-9">Note</div>
-							</span>
-						</div>
-					</div>
-					<div className="flex flex-col items-stretch w-[79%] ml-5 mt-5 max-md:w-full max-md:ml-0">
-						<span className="items-stretch flex flex-col my-auto max-md:max-w-full max-md:mt-10">
-							<div className="text-black text-xs leading-5 max-md:max-w-full">Losartan</div>
-							<div className="text-black text-xs leading-5 mt-9 max-md:max-w-full">Losartan</div>
-							<div className="text-black text-xs leading-5 mt-9 max-md:max-w-full">50 mg</div>
-							<div className="text-black text-xs leading-5 mt-9 max-md:max-w-full">Tablet</div>
-							<div className="text-black text-xs leading-5 mt-9 max-md:max-w-full">30</div>
-							<div className="text-black text-xs leading-5 mt-9 max-md:max-w-full">
-								<ul>
-									<li>1 tablet 1x/ day for 30 days</li>
-									<li>
-										30 minutes of moderate-intensity exercise (e.g., brisk walking, swimming, or cycling) at least five
-										days a week.
-									</li>
-								</ul>
-							</div>
-						</span>
-					</div>
-				</div>
-			</div>
+			<table className="max-w-fit border-spacing-y-7 border-separate">
+				{cplan.map((item) => (
+					<tr key={item.variable}>
+						<td className="w-5">
+							<Image alt="picture" height={0} width={0} loading="lazy" src={item.imgsrc} className="w-5" />
+						</td>
+						<td className="border-l-[16px] border-transparent">
+							<div className="text-black text-xs font-semibold leading-5 self-center my-auto">{item.variable}</div>
+						</td>
+						<td className="border-l-[5rem] border-transparent">
+							{item.value.map((val) => (
+								<div key={val} className="text-black text-xs leading-5 ml-auto">
+									{val}
+								</div>
+							))}
+						</td>
+					</tr>
+				))}
+			</table>
 		</div>
 	);
 }
