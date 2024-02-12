1d6a9bdb6faf904952afabe68e5b54ee0040733b
Lastronus <kenji.hartigan.go@gmail.com>
2024-02-13 00:57:29 +0800

    Fixed referral lines

--- src/app/referral/components/referralDoctor.jsx
+++ src/app/referral/components/referralDoctor.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const ReferralDoctors= ({ name, age, onClick }) => {
   return (
-    <div className="ml-10 flex w-full flex-col">
+    <div className="ml-5 flex w-full flex-col">
       <div className="flex w-full items-stretch justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
         <div className="flex items-stretch justify-between gap-5">
                 <div className="flex items-center my-4">
@@ -28,7 +28,7 @@ const ReferralDoctors= ({ name, age, onClick }) => {
         <div className="self-center flex aspect-[3.3333333333333335] flex-col justify-center items-stretch my-auto">
         </div>
       </div>
-      <div className="bg-gray-400 self-stretch min-h-[1px] w-full mt-2 mb-2 max-md:max-w-full " />
+      <div className="bg-gray-400 self-stretch min-h-[1px] w-[98.4%] mt-2 mb-2" />
     </div>
   );
 };
