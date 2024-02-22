b17d632ac5b42d6d21184408bdeb70bd37e8272d
yanajuson <kay_eliana_juson@dlsu.edu.ph>
2024-02-22 15:04:56 +0800

    Patient screens + remaining from doctor's end

--- src/app/health_records/about_patient/components/diagnosesDashboard.jsx
+++ src/app/health_records/about_patient/components/diagnosesDashboard.jsx
@@ -1,91 +1,90 @@
-
 import Image from "next/image";
 import {
-    Table,
-    TableHeader,
-    TableBody,
-    TableColumn,
-    TableRow,
-    TableCell
-  } from "@nextui-org/react";
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
 
 import * as React from "react";
-  
+import ViewDiagnosis from "./sub_components/viewDiagnosis";
+
 export default function MasterData() {
-    const mData = [
-        {
-            variable: "Diagnoses",
-            value: "Type 2 Diabetes Mellitus",
-        },
-        {
-            variable: "Date of Diagnosis",
-            value: "2020-01-10",
-        },
-        {
-            variable: "Status",
-            value: "Being Managed",
-        },
-        {
-            variable: "Doctor",
-            value: "Dr. John  Doe",
-        },
-        {
-            variable: "Hospital",
-            value: "PGH",
-        },
+  const [currentPage, setCurrentPage] = React.useState(0);
 
-    ];
-    return (
+  const mData = [
+    {
+      variable: "Diagnoses",
+      value: "Type 2 Diabetes Mellitus",
+      handler: () => setCurrentPage(currentPage + 1),
+    },
+    {
+      variable: "Date of Diagnosis",
+      value: "2020-01-10",
+    },
+    {
+      variable: "Status",
+      value: "Being Managed",
+    },
+    {
+      variable: "Doctor",
+      value: "Dr. John  Doe",
+    },
+    {
+      variable: "Hospital",
+      value: "PGH",
+    },
+  ];
+
+  return (
+    <>
+      {currentPage === 0 && (
         <>
-            <div className="text-black text-base font-bold leading-5 mt-8 mb-1 max-md:ml-1 max-md:mt-10">
-				DIAGNOSES
-			</div>
+          <div className="text-black text-base font-bold leading-5 mt-8 mb-1 max-md:ml-1 max-md:mt-10">
+            DIAGNOSES
+          </div>
 
-            <table className="pt-1.5 text-xs leading-5 text-black mt-10 max-w-[914px]">
-                <thead>
-                    <tr className="font-medium text-left">
-                        {mData.map((item, index) => (
-                            <th key={index}>{item.variable}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        {mData.map((item, index) => (
-                            <td key={index} className={`${index === 0 ? 'font-normal' : 'mt-8'}`}>
-                                {typeof item.value === 'object' ? (
-                                    <button
-                                        className="font-semibold text-sky-900 rounded px-5 py-1.5 border border-sky-900"
-                                        onClick={item.value.onClick}
-                                    >
-                                        {item.value.label}
-                                    </button>
-                                ) : (
-                                    item.value
-                                )}
-                            </td>
-                        ))}
-                    </tr>
-                </tbody>
-            </table>
-
-            { /* BACK BUTTON */ }
-            <div className="flex flex-col items-start justify-end text-xs font-semibold text-black whitespace-nowrap rounded max-w-[137px] mt-10">
-                <button className="flex items-center justify-center px-10 py-1 w-full rounded border border-sky-900 border-solid font-semibold border-1.5">
-                    <div className="flex gap-0.5 justify-between items-center">
-                    <Image 
-                    height={0}
-                    width={0}
-                        loading="lazy"
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/0de7471415fd70bdaba9dd1e6f7c2e7075e37988a454dfb91c7aed9b11350077?"
-                        className="w-4 h-4 aspect-square"
-                        alt="Back Arrow"
-                    />
-                    <div className="ml-1">BACK</div>
-                    </div>
-                </button>
-            </div>
+          <table className="pt-1.5 text-xs leading-5 text-black mt-10 max-w-[914px]">
+            <thead>
+              <tr className="font-medium text-left">
+                {mData.map((item, index) => (
+                  <th key={index}>{item.variable}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {mData.map((item, index) => (
+                  <td
+                    key={index}
+                    className={`${index === 0 ? "font-normal" : "mt-8"}`}
+                  >
+                    {item.variable === "Diagnoses" ? (
+                      <button onClick={item.handler}>
+                        {typeof item.value === "object" ? (
+                          <button
+                            className="font-semibold text-sky-900 rounded px-5 py-1.5 border border-sky-900"
+                            onClick={item.value.onClick}
+                          >
+                            {item.value.label}
+                          </button>
+                        ) : (
+                          item.value
+                        )}
+                      </button>
+                    ) : (
+                      item.value
+                    )}
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
         </>
-      );
+      )}
+      {currentPage === 1 && <ViewDiagnosis />}
+    </>
+  );
 }
-
--- src/app/patient/careplan_list/components/laboratoryTests.jsx
+++ src/app/patient/careplan_list/components/laboratoryTests.jsx
@@ -1,98 +1,110 @@
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function LabTests() {
-	const router = useRouter();
-	const cPList = [
-		{
-			date: "2023-01-07",
-			labtest: "2D ECHO",
-			request: "Dr. John Doe",
-			special: "Endocrinology",
-			status: "Uploaded",
-		},
-		{
-			date: "2023-01-07",
-			labtest: "2D ECHO",
-			request: "Dr. Johnny Santos",
-			special: "Cardiology",
-			status: "Not yet started",
-		},
-		{
-			date: "2023-01-07",
-			labtest: "2D ECHO",
-			request: "Dr. John Doe",
-			special: "Endocrinology",
-			status: "Uploaded",
-		},
-		{
-			date: "2023-01-07",
-			labtest: "2D ECHO",
-			request: "Dr. John Doe",
-			special: "Endocrinology",
-			status: "Uploaded",
-		},
-		{
-			date: "2023-01-07",
-			labtest: "2D ECHO",
-			request: "Dr. John Doe",
-			special: "Endocrinology",
-			status: "Uploaded",
-		},
-		{
-			date: "2023-01-07",
-			labtest: "2D ECHO",
-			request: "Dr. John Doe",
-			special: "Endocrinology",
-			status: "Uploaded",
-		},
-		{
-			date: "2023-01-07",
-			labtest: "2D ECHO",
-			request: "Dr. John Doe",
-			special: "Endocrinology",
-			status: "Uploaded",
-		},
-	];
+  const router = useRouter();
+  const cPList = [
+    {
+      date: "2023-01-07",
+      labtest: "2D ECHO",
+      request: "Dr. John Doe",
+      special: "Endocrinology",
+      status: "Uploaded",
+    },
+    {
+      date: "2023-01-07",
+      labtest: "2D ECHO",
+      request: "Dr. Johnny Santos",
+      special: "Cardiology",
+      status: "Not yet started",
+    },
+    {
+      date: "2023-01-07",
+      labtest: "2D ECHO",
+      request: "Dr. John Doe",
+      special: "Endocrinology",
+      status: "Uploaded",
+    },
+    {
+      date: "2023-01-07",
+      labtest: "2D ECHO",
+      request: "Dr. John Doe",
+      special: "Endocrinology",
+      status: "Uploaded",
+    },
+    {
+      date: "2023-01-07",
+      labtest: "2D ECHO",
+      request: "Dr. John Doe",
+      special: "Endocrinology",
+      status: "Uploaded",
+    },
+    {
+      date: "2023-01-07",
+      labtest: "2D ECHO",
+      request: "Dr. John Doe",
+      special: "Endocrinology",
+      status: "Uploaded",
+    },
+    {
+      date: "2023-01-07",
+      labtest: "2D ECHO",
+      request: "Dr. John Doe",
+      special: "Endocrinology",
+      status: "Uploaded",
+    },
+  ];
 
-	return (
-		<>
-			<table className="min-w-full divide-y divide-gray-200 mt-10">
-				<thead className="bg-gray-50">
-					<tr>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							Date Requested
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							Laboratory Test
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							Requested By
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							Specialization
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-					</tr>
-				</thead>
-				<tbody className="bg-white divide-y divide-gray-200">
-					{cPList.map((item) => (
-						<tr
-							key={item.date}
-							onClick={() => {
-								router.push("careplan_list/labtest");
-							}}
-						>
-							<td className="px-6 py-4 whitespace-nowrap">{item.date}</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								<button className="text-blue-500 underline hover:text-blue-700 hover:underline">{item.labtest}</button>
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">{item.request}</td>
-							<td className="px-6 py-4 whitespace-nowrap">{item.special}</td>
-							<td className="px-6 py-4 whitespace-nowrap">{item.status}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</>
-	);
+  const lab = [
+    {
+      srcmedicine:
+        "https://cdn.builder.io/api/v1/image/assets/TEMP/4a525f62acf85c2276bfc82251c6beb10b3d621caba2c7e3f2a4701177ce98c2?",
+      labname: "Fasting Blood Sugar (FBS) Test",
+      reqdate: "2024/01/24",
+    },
+    {
+      srcmedicine:
+        "https://cdn.builder.io/api/v1/image/assets/TEMP/4a525f62acf85c2276bfc82251c6beb10b3d621caba2c7e3f2a4701177ce98c2?",
+      labname: "HbA1c Test",
+      reqdate: "2024/01/24",
+    },
+    {
+      srcmedicine:
+        "https://cdn.builder.io/api/v1/image/assets/TEMP/4a525f62acf85c2276bfc82251c6beb10b3d621caba2c7e3f2a4701177ce98c2?",
+      labname: "Total Cholesterol Test",
+      reqdate: "2024/01/24",
+    },
+  ];
+  return (
+    <>
+      <div className=" bg-white flex flex-col items-stretch min-h-screen w-full">
+        <div className="w-full max-md:max-w-full h-full">
+          {lab.map((lab, index) => (
+            <button
+              key={index}
+              className="flex flex-col mt-10 items-start text-xs leading-5 text-black max-w-[601px]"
+            >
+              <div className="flex gap-3.5 font-semibold whitespace-nowrap">
+                <Image
+                  alt="image"
+                  height={0}
+                  width={0}
+                  loading="lazy"
+                  src={lab.srcmedicine}
+                  className="aspect-square fill-black w-[15px]"
+                />
+                <div className="my-auto">{lab.labname}</div>
+              </div>
+              <div className="flex gap-5 justify-between ml-7 max-md:ml-2.5">
+                <div className="flex gap-1 justify-between font-medium whitespace-nowrap">
+                  Requested on:
+                  <div className="grow my-auto">{lab.reqdate}</div>
+                </div>
+              </div>
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
