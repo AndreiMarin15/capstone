c1b158357d71576105e6a1b0bf6af092d7b4e1f6
nicoletabije <nicole_tabije@dlsu.edu.ph>
2024-02-24 12:57:56 +0800

    updated allergies

--- src/app/health_records/about_patient/components/masterDataDashboard.jsx
+++ src/app/health_records/about_patient/components/masterDataDashboard.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 
 export default function MasterData() {
   const router = useRouter(); 
-  const [currentPage, setCurrentPage] = useState(1);
 
   const mData = [
     {
