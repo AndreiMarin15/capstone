bd759f274a522fac0c8e635bf1dd299871904edc
Andrei <misael_marin@dlsu.edu.ph>
2024-02-27 00:13:23 +0800

    update_back_button

--- src/app/require_auth/page.jsx
+++ src/app/require_auth/page.jsx
@@ -2,13 +2,18 @@
 import React, { useState } from "react"; // <-- Import useState from React
 import Image from "next/image";
 import ErrorPic from "../assets/nothing-found.png";
+import { useRouter } from "next/navigation";
 
 export default function AuthError() {
+  const router = useRouter()
   return (
-    <div className="border h-full w-full bg-white flex flex-col items-center px-20 py-12 border-solid border-stone-300 max-md:px-5">
+    <div className=" h-full w-full bg-white flex flex-col items-center px-20 py-12  max-md:px-5">
       <div className="flex flex-col h-full items-center px-5 font-semibold max-w-[545px]">
         <Image
           loading="lazy"
+          alt="image"
+          height={0}
+          width={0}
           src={ErrorPic}
           className="max-w-full aspect-square w-[135px]"
         />
@@ -18,10 +23,18 @@ export default function AuthError() {
           Please sign up or log in to proceed.
         </div>
         <div className="flex gap-5 justify-between mt-12 max-w-full text-lg text-white whitespace-nowrap w-[323px] max-md:mt-10">
-          <button className="grow justify-center px-8 py-3 bg-sky-900 rounded max-md:px-5">
+          <button
+          onClick={() => {
+            router.push("/")
+          }}
+          className="grow justify-center px-8 py-3 bg-sky-900 rounded max-md:px-5">
             Sign Up
           </button>
-          <button className="grow justify-center px-10 py-3 bg-sky-900 rounded max-md:px-5">
+          <button 
+          onClick={() => {
+            router.push("/login")
+          }}
+          className="grow justify-center px-10 py-3 bg-sky-900 rounded max-md:px-5">
             Log In
           </button>
         </div>
