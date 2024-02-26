c7ade343cc2c03b7395a3ffa4a96c6e2f20f72cd
Andrei <misael_marin@dlsu.edu.ph>
2024-02-26 18:25:39 +0800

    update_providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -11,7 +11,7 @@ import { currentUser } from "./store";
 export function Providers({ children }: { children: React.ReactNode }) {
 	const router = useRouter();
 	const pathname = usePathname();
-	const current = currentUser()
+	const current = currentUser();
 	useEffect(() => {
 		const getAuth = async () => {
 			const authData = await authentication.getSession();
@@ -19,12 +19,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
 				console.error(authData.message);
 				return;
 			}
-			// if (!authData?.session?.user.id) {
-			// 	if (pathname !== "/" && pathname !== "/login" && pathname !== "/patient_form" && pathname !== "/doctor_form") {
-			// 		router.push("/");
-			// 	}
-			// } 
-			else {
+			if (!authData?.session?.user.id) {
+				if (pathname !== "/" && pathname !== "/login" && pathname !== "/patient_form" && pathname !== "/doctor_form") {
+					router.push("/");
+				}
+			} else {
 				if (pathname === "/" || pathname === "/login" || pathname === "/patient_form" || pathname === "/doctor_form") {
 					if (current.user.type === "patient") {
 						router.push("/patient/dashboard");
