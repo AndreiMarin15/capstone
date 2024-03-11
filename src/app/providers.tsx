1d3182b1a249352979f949d8de119b1b00ce192a
Lastronus <kenji.hartigan.go@gmail.com>
2024-03-12 02:45:09 +0800

    Getauth

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -41,7 +41,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 			}
 		};
 
-		// getAuth();
+		getAuth();
 	}, [router, pathname, current.user]);
 	return <NextUIProvider>{children}</NextUIProvider>;
 }
