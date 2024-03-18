4c0c1a7a7a6352c09859ab0513a5d63e39a11ab3
nicoletabije <nicole_tabije@dlsu.edu.ph>
2024-03-18 18:14:35 +0800

    Added Messaging feature
    Only on doctor side

--- src/app/patient/messages/page.jsx
+++ src/app/patient/messages/page.jsx
@@ -1,14 +1,77 @@
 import * as React from "react";
 import Image from "next/image";
+import {
+  getMessages,
+  getMessagesAndSubscribe,
+} from "../../../../lib/backend/message/getMessages";
 export default function Messaging() {
+  const [newMessage, setNewMessage] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+  const [chat, setChat] = React.useState("");
+  const [chats, setChats] = React.useState([
+    { id: "", chat: "", full_name: "" },
+  ]);
+
+  const [messageInfo, setMessageInfo] = React.useState({
+    messages: [
+      {
+        message: "No data",
+        created_at: "2024-03-17T09:20:29.080437+00:00",
+        message_status: "received",
+      },
+      {
+        message: "No data",
+        created_at: "2024-03-17T09:22:08.343375+00:00",
+        message_status: "sent",
+      },
+    ],
+    patients: {
+      last_name: "Undefined",
+      first_name: "Undefined",
+    },
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const message = formData.get("message");
+    setMessage("");
+    if (patient) {
+      getMessages.insertMessage(patient, message, "sent");
+    }
+  };
+  const handleInserts = (payload) => {
+    setNewMessage(!newMessage);
+  };
+
+  getMessagesAndSubscribe(handleInserts);
+
+  //change to get chat based on doctor if not exist create a chat
+  React.useEffect(() => {
+    const importMessage = async () => {
+      const chatList = await getMessages.getChats();
+      setChats(chatList);
+      setPatient(chatList[0]?.id || ""); // Set the initial patient ID
+    };
+
+    importMessage();
+  }, [newMessage]);
+
+  //get the chats under the doctor
+  React.useEffect(() => {
+    const importMessage = async () => {
+      const messages = await getMessages.getMessage(chat);
+      // update to read
+      await getMessages.updateRead(patient, "sent", "seen");
+      setMessageInfo(messages);
+    };
+
+    importMessage();
+  }, [changeUser, newMessage]);
   return (
     <div className="bg-white flex flex-col items-end max-md:pl-5 h-[100vh]">
       <div className=" shadow-sm flex gap-5 justify-between px-14 py-9 mt-1.5 w-full whitespace-nowrap bg-white max-md:flex-wrap max-md:px-5 max-md:max-w-full">
-        <div className="mt-2.5 text-xl font-semibold text-black">
-          Messages
-        </div>
-        <div className="flex gap-3.5 justify-between text-xs">
-        </div>
+        <div className="mt-2.5 text-xl font-semibold text-black">Messages</div>
+        <div className="flex gap-3.5 justify-between text-xs"></div>
       </div>
       <div className="shadow-sm bg-white mt-0 w-full max-w-full pl-5 pr-20 pt-8 pb-4 max-md:px-5">
         <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
@@ -41,12 +104,16 @@ export default function Messaging() {
               className="aspect-square object-contain object-center w-7 overflow-hidden shrink-0 max-w-full"
             />
             <span className="text-zinc-600 text-xs font-medium leading-5 shadow-sm bg-white self-stretch grow justify-center items-stretch px-5 py-4 rounded max-md:max-w-full max-md:px-5">
-            {"Hi, how are you dealing with the care plan? Do you have any concerns?"}
+              {
+                "Hi, how are you dealing with the care plan? Do you have any concerns?"
+              }
             </span>
           </div>
           <div className="justify-end items-stretch flex gap-4 mt-12 self-end max-md:max-w-full max-md:flex-wrap max-md:mt-10">
             <span className="text-white text-xs font-medium leading-5 shadow-sm bg-blue-500 grow justify-center items-stretch p-4 rounded max-md:max-w-full">
-              {"Yes, regarding my diet. I think our weight goal is too challenging for me. It makes me dizzy."}
+              {
+                "Yes, regarding my diet. I think our weight goal is too challenging for me. It makes me dizzy."
+              }
             </span>
             <Image
               alt="picture"
@@ -68,23 +135,34 @@ export default function Messaging() {
             className="aspect-square object-contain object-center w-7 overflow-hidden shrink-0 max-w-full"
           />
           <span className="text-zinc-600 text-xs font-medium leading-5 shadow-sm bg-white self-stretch grow justify-center items-stretch px-5 py-4 rounded max-md:max-w-full max-md:px-5">
-          {"Okay. I'll adjust them as necessary. Kindly view your adjusted care plan in a few minutes."}
+            {
+              "Okay. I'll adjust them as necessary. Kindly view your adjusted care plan in a few minutes."
+            }
           </span>
         </div>
       </div>
-      <div className="shadow-sm bg-white self-center flex w-full max-w-full flex-col items-stretch px-12 py-3.5 max-md:max-w-full max-md:px-5">
+      <form
+        onSubmit={handleSubmit}
+        className="shadow-sm bg-white self-center flex w-full max-w-full flex-col items-stretch px-12 py-3.5 max-md:max-w-full max-md:px-5"
+      >
         <input
           className="text-zinc-500 text-base leading-6 whitespace-nowrap bg-stone-50 pl-5 pr-16 pt-3.5 pb-14 rounded-lg items-start max-md:max-w-full max-md:pr-5"
           placeholder=" Message..."
+          name="message"
+          onChange={handleMessageChange}
+          value={message}
+          required
         />
         <div className="flex w-full items-center justify-between gap-5 mt-2.5 pr-4 max-md:max-w-full max-md:flex-wrap">
-          <span className="flex items-stretch gap-2 my-auto">
-          </span>
-          <button className="text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-blue-500 self-stretch px-7 py-2 rounded max-md:px-5">
+          <span className="flex items-stretch gap-2 my-auto"></span>
+          <button
+            type="submit"
+            className="text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-blue-500 self-stretch px-7 py-2 rounded max-md:px-5"
+          >
             SEND
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
