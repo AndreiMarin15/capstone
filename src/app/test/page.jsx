3c0054700aa8a4697fa00d0f09278ddb77cd8904
Andrei <misael_marin@dlsu.edu.ph>
2024-03-21 16:23:06 +0800

    update

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,82 +1,77 @@
-// "use client";
+"use client";
 
-// import { PROJECT } from "../../../lib/backend/project/db";
-// import { PUBLIC } from "../../../lib/backend/public/db";
-// import { useEffect, useState } from "react";
-// export default function Notes() {
-// 	const [otp, setOtp] = useState(null);
-	
-// 	const [userNumber, setNumber] = useState(null);
-	
+import { PROJECT } from "../../../lib/backend/project/db";
+import { PUBLIC } from "../../../lib/backend/public/db";
+import { useEffect, useState } from "react";
+export default function Notes() {
+	// const [otp, setOtp] = useState(null);
 
-// 	const sendOTP = () => {
-// 		const myHeaders = new Headers();
-// 		myHeaders.append("Authorization", "App 78aafa3855b42fc87b6336514b2447a6-00e11e65-977b-4589-b0ac-2814b265773a");
-// 		myHeaders.append("Content-Type", "application/json");
-// 		myHeaders.append("Accept", "application/json");
-// 		console.log(userNumber);
-// 		const raw = JSON.stringify({
-// 			messages: [
-// 				{
-// 					destinations: [{ to: userNumber }],
-// 					from: "ServiceSMS",
-// 					text: `Hello! Your OTP is  ${otp}
-					
-// 					By providing this pin to your healthcare provider, you are authorizing EndoTracker and [NAME OF DOCTOR], to access your health information, particularly the following:
+	// const [userNumber, setNumber] = useState(null);
 
-// 					- SAMPLE DATA PULL 1
-// 					- SAMPLE DATA PULL 2
-					
-// 					EndoTracker respects the privacy of personal data, and are committed to handling your personal data with care. It is your right to be informed of how EndoTracker collects your data, including the purposes of how we collect, use, and disclose. 
-					
-// 					For more information on how EndoTracker handles and makes use of your data, please refer to the Privacy Policy full text which can be found in the system https://capstone-cap2224.vercel.app/legal/privacy_policy.`,
-// 				},
-// 			],
-// 		});
+	// const sendOTP = () => {
+	// 	const myHeaders = new Headers();
+	// 	myHeaders.append("Authorization", "App 78aafa3855b42fc87b6336514b2447a6-00e11e65-977b-4589-b0ac-2814b265773a");
+	// 	myHeaders.append("Content-Type", "application/json");
+	// 	myHeaders.append("Accept", "application/json");
+	// 	console.log(userNumber);
+	// 	const raw = JSON.stringify({
+	// 		messages: [
+	// 			{
+	// 				destinations: [{ to: userNumber }],
+	// 				from: "ServiceSMS",
+	// 				text: `Hello! Your OTP is  ${otp}
 
-// 		const requestOptions = {
-// 			method: "POST",
-// 			headers: myHeaders,
-// 			body: raw,
-// 			redirect: "follow",
-// 		};
+	// 				By providing this pin to your healthcare provider, you are authorizing EndoTracker and [NAME OF DOCTOR], to access your health information, particularly the following:
 
-// 		fetch("https://y36nrg.api.infobip.com/sms/2/text/advanced", requestOptions)
-// 			.then((response) => response.text())
-// 			.then((result) => console.log(result))
-// 			.catch((error) => console.error(error));
-// 	};
+	// 				- SAMPLE DATA PULL 1
+	// 				- SAMPLE DATA PULL 2
 
-	
+	// 				EndoTracker respects the privacy of personal data, and are committed to handling your personal data with care. It is your right to be informed of how EndoTracker collects your data, including the purposes of how we collect, use, and disclose.
 
-	
+	// 				For more information on how EndoTracker handles and makes use of your data, please refer to the Privacy Policy full text which can be found in the system https://capstone-cap2224.vercel.app/legal/privacy_policy.`,
+	// 			},
+	// 		],
+	// 	});
 
-// 	return (
-// 		<>
-// 			<input
-// 				className="border-2 border-black"
-// 				type="text"
-// 				onChange={(e) => {
-// 					setEntered(e.target.value);
-// 				}}
-// 			/>
-// 			<input
-// 				className="border-2 border-black"
-// 				type="text"
-// 				onChange={(e) => {
-// 					setNumber(e.target.value);
-// 				}}
-// 			/>
+	// 	const requestOptions = {
+	// 		method: "POST",
+	// 		headers: myHeaders,
+	// 		body: raw,
+	// 		redirect: "follow",
+	// 	};
 
-// 			<button
-// 				onClick={() => {
-// 					sendOTP();
-// 				}}
-// 			>
-// 				OTP
-// 			</button>
+	// 	fetch("https://y36nrg.api.infobip.com/sms/2/text/advanced", requestOptions)
+	// 		.then((response) => response.text())
+	// 		.then((result) => console.log(result))
+	// 		.catch((error) => console.error(error));
+	// };
 
-// 			{enteredOtp === otp ?? <p>OTP CORRECT</p>}
-// 		</>
-// 	);
-// }
+	return (
+		<>
+			{/* <input
+				className="border-2 border-black"
+				type="text"
+				onChange={(e) => {
+					setEntered(e.target.value);
+				}}
+			/>
+			<input
+				className="border-2 border-black"
+				type="text"
+				onChange={(e) => {
+					setNumber(e.target.value);
+				}}
+			/>
+
+			<button
+				onClick={() => {
+					sendOTP();
+				}}
+			>
+				OTP
+			</button>
+
+			{enteredOtp === otp ?? <p>OTP CORRECT</p>} */}
+		</>
+	);
+}
