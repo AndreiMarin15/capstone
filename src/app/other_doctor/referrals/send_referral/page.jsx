76fd4ad51c974707a577dec6ab2a5689d347810c
Andrei <misael_marin@dlsu.edu.ph>
2024-03-25 22:10:29 +0800

    update

--- src/app/other_doctor/referrals/send_referral/page.jsx
+++ src/app/other_doctor/referrals/send_referral/page.jsx
@@ -4,150 +4,158 @@ import * as React from "react";
 import ProgressBar from "../components/otherDoctorProgressBar";
 import { useRouter } from "next/navigation";
 import ReferralPatients from "../components/otherDoctorReferralPatients";
+import ReferralDoctors from "../components/otherDoctorReferralDoctor";
 import NotesAndReview from "../components/otherDoctorNotesAndReview";
-const navigation = [
-  {
-    name: "DELA CRUZ, Juan",
-    age: "70",
-    href: "",
-    src: "",
-  },
-  {
-    name: "RIZAL, Jose",
-    age: "43",
-    href: "",
-    src: "",
-  },
-  {
-    name: "BONIFACIO, Andres",
-    age: "39",
-    href: "",
-    src: "",
-  },
-  {
-    name: "QUEZON, Manuel",
-    age: "44",
-    href: "",
-    src: "",
-  },
-  {
-    name: "SORA, Tandang",
-    age: "61",
-    href: "",
-    src: "",
-  },
-  {
-    name: "LUNA, Juan",
-    age: "30",
-    href: "",
-    src: "",
-  },
-  {
-    name: "LUNA, Antonio",
-    age: "18",
-    href: "",
-    src: "",
-  },
-];
-
-const navigation2 = [
-  {
-    name: "SKYWALKER, Anakin",
-    age: "71",
-    href: "",
-    src: "",
-  },
-  {
-    name: "SOLO, Han",
-    age: "43",
-    href: "",
-    src: "",
-  },
-  {
-    name: "KENOBI, Obi",
-    age: "39",
-    href: "",
-    src: "",
-  },
-  {
-    name: "REN, Kylo",
-    age: "44",
-    href: "",
-    src: "",
-  },
-];
+import retrieveReferralData from "../../../../../lib/backend/referral/retrieveReferralData";
+import sendReferralData from "../../../../../lib/backend/referral/sendReferralData";
 
 export default function SendReferral() {
-  const router = useRouter();
-  const [currentState, setCurrentState] = React.useState (1);
+	const router = useRouter();
+	const [patients, setPatients] = React.useState([]);
 
-  const handleSelect = () => {
-    if (currentState < 3) {
-      setCurrentState((currentState + 1)  );
-    }
-    console.log(currentState);
-  };
+	const [doctors, setDoctors] = React.useState([]);
 
-  const handleBack = () => {
-    if (currentState > 1) {
-      setCurrentState((currentState - 1)  );
-    }
-  };
+	const [currentState, setCurrentState] = React.useState(1);
+	const [selectedPatientId, setSelectedPatientId] = React.useState(null);
+	const [selectedDoctorId, setSelectedDoctorId] = React.useState(null);
 
-  return (
-    <div className="border bg-white flex flex-col items-stretch pb-8 border-solid border-stone-300 h-[120vh]">
-      <div className="ml-6 mt-8 text-black text-xl font-semibold leading-8">Referral</div>
-      <ProgressBar currentStep={currentState} />
-      <>
-        <div className={currentState === 3 ? "pb-20" : ""}>
-          {currentState === 1 ? (
-            navigation.map((item) => (
-              <ReferralPatients key={item.name} name={item.name} age={item.age} onClick={handleSelect} />
-            ))
-          ) : (<NotesAndReview />
-          
-          )}
-        </div>
-      </>
-      <div className="w-full flex justify-between px-14 max-md:max-w-full max-md:px-5">
-        {currentState > 1 ? (
-          <button
-            onClick={() => {
-              if (currentState > 1) {
-                setCurrentState((currentState - 1)  );
-              }
-            }}
-            className="mt-5 text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-gray-400 self-stretch mr-2 px-6 py-2 rounded max-md:px-3"
-          >
-            BACK
-          </button>
-        ) : (
-          <div></div>
-        )}
+	const [selectedPatient, setSelectedPatient] = React.useState({});
+	const [selectedDoctor, setSelectedDoctor] = React.useState({});
 
-        {currentState === 1 && (
-          <button
-          onClick={() => {
-            router.push("/referral");
-          }}
-          className="mt-5 text-white text-xs font-semibold whitespace-nowrap items-stretch bg-gray-400 mr-2 px-6 py-2 rounded"
-          style={{ marginRight: 'auto' }}
-        >
-          BACK
-        </button>
-        )}  
+	const [notes, setNotes] = React.useState("");
 
-        <button
-          onClick={() => {
-            if (currentState < 3) {
-              setCurrentState((currentState + 1) );
-            } 
-          }}
-          className={`mt-5 text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-sky-900 self-stretch mr-2 px-6 py-2 rounded max-md:px-3`}
-          >
-          {currentState === 3 ? "SEND" : "NEXT"}
-        </button>
-      </div>
-    </div>
-  );
+	const handleSelect = () => {
+		if (currentState < 3) {
+			setCurrentState(currentState + 1);
+		}
+		console.log(currentState);
+	};
+
+	const handleBack = () => {
+		if (currentState > 1) {
+			setCurrentState(currentState - 1);
+		}
+	};
+
+	React.useEffect(() => {
+		console.log(currentState);
+	}, [currentState]);
+
+	React.useEffect(() => {
+		console.log(patients, doctors);
+	}, [patients, doctors]);
+	React.useEffect(() => {
+		const fetchData = async () => {
+			const patients = await retrieveReferralData.getPatients();
+			const doctors = await retrieveReferralData.getDoctors();
+
+			console.log(patients);
+
+			console.log(doctors);
+			setPatients(patients);
+			setDoctors(doctors);
+		};
+
+		fetchData();
+	}, []);
+
+	
+
+	return (
+		<div className="bg-white flex flex-col items-stretch pb-8 h-[100vh]">
+			<div className="ml-6 mt-8 text-black text-xl font-semibold leading-8">Referral</div>
+			<ProgressBar currentStep={currentState} />
+			<>
+				<div className={(currentState === 3 ? "pb-20" : "") + " overflow-auto"}>
+					{patients.length > 0 && doctors.length > 0 ? (
+						<>
+							{currentState === 1 ? (
+								patients.map((item) => (
+									<div key={item.id}>
+										<div
+											onClick={() => {
+												setSelectedPatientId(item.id);
+												setSelectedPatient(item);
+											}}
+										>
+											<ReferralPatients name={item.name} age={item.age} id={item.id} selectedId={selectedPatientId} />
+										</div>
+									</div>
+								))
+							) : currentState === 2 ? (
+								doctors.map((item) => (
+									<div key={item.id}>
+										<div
+											onClick={() => {
+												setSelectedDoctorId(item.id);
+												setSelectedDoctor(item);
+											}}
+										>
+											<ReferralDoctors name={item.name} age={item.age} id={item.id} selectedId={selectedDoctorId} />
+										</div>
+									</div>
+								))
+							) : (
+								<NotesAndReview
+									selectedDoctor={selectedDoctor}
+									selectedPatient={selectedPatient}
+									notes={notes}
+									setNotes={setNotes}
+								/>
+							)}
+						</>
+					) : (
+						<>Loading Data...</>
+					)}
+				</div>
+			</>
+			<div className="w-full flex justify-between px-14 max-md:max-w-full max-md:px-5">
+				{currentState > 1 ? (
+					<button
+						onClick={() => {
+							if (currentState > 1) {
+								setCurrentState(currentState - 1);
+							}
+						}}
+						className="mt-5 text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-gray-400 self-stretch mr-2 px-6 py-2 rounded max-md:px-3"
+					>
+						BACK
+					</button>
+				) : (
+					<div></div>
+				)}
+
+				{currentState === 1 && (
+					<button
+						onClick={() => {
+							router.push("/other_doctor/referrals");
+						}}
+						className="mt-5 text-white text-xs font-semibold whitespace-nowrap items-stretch bg-gray-400 mr-2 px-6 py-2 rounded"
+						style={{ marginRight: "auto" }}
+					>
+						BACK
+					</button>
+				)}
+
+				<button
+					onClick={() => {
+						if (currentState < 3) {
+							setCurrentState(currentState + 1);
+						} else if (currentState === 3) {
+              const referralData = {
+                patient_id: selectedPatientId,
+                doctor_id: selectedDoctorId,
+                notes: notes,
+              };
+              sendReferralData.newReferralRequest(referralData);
+              router.push("/referral");
+						}
+					}}
+					className={`mt-5 text-white text-xs font-semibold whitespace-nowrap justify-center items-stretch bg-sky-900 self-stretch mr-2 px-6 py-2 rounded max-md:px-3`}
+				>
+					{currentState === 3 ? "SEND" : "NEXT"}
+				</button>
+			</div>
+		</div>
+	);
 }
