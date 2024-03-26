c152b89a716d9c4c8925fd81567d68647f3591a8
nicoletabije <nicole_tabije@dlsu.edu.ph>
2024-03-27 00:07:51 +0800

    care Plan added

--- src/app/health_records/about_patient/[patient_id]/page.jsx
+++ src/app/health_records/about_patient/[patient_id]/page.jsx
@@ -104,7 +104,7 @@ export default function AboutPatient({ params }) {
                 ) : selected === "Medications" ? (
                   <Medications patientId={patientId} />
                 ) : selected === "Care Plans" ? (
-                  <CarePlans patientId={patientId} />
+                  <CarePlans patientId={patientId} patientData={patientData} />
                 ) : selected === "Lab Tests" ? (
                   <LabTests />
                 ) : selected === "Vitals & Biometrics" ? (
