d650efd163b4b6af0358f2e9f03ca0d16315f159
yanajuson <kay_eliana_juson@dlsu.edu.ph>
2024-03-30 16:53:54 +0800

    socialhistoryedit

--- src/app/patient/personal_details/components/family&socialHistoryDashboard.jsx
+++ src/app/patient/personal_details/components/family&socialHistoryDashboard.jsx
@@ -3,15 +3,22 @@ import { useState, useEffect } from "react";
 import FamilyHistory from "./sub_components/viewfamilyHistory";
 import { FaM } from "react-icons/fa6";
 import { getFamilyAndSocialHistory } from "../../../../../lib/backend/patient/personal_details/master_data";
-import EditFamilyHistory  from "./sub_components/editFamilyHistory";
-import EditSocialHistory  from "./sub_components/editSocialHistory";
-
+import EditFamilyHistory from "./sub_components/editFamilyHistory";
 
 export default function SocialHistory() {
+  const [isEditingSocialHistory, setIsEditingSocialHistory] = useState(false);
+  const [originalSocialHistory, setOriginalSocialHistory] = useState({}); // Store original data
+
+  const disabledInputStyle = {
+    backgroundColor: "#ccc",
+    color: "#666",
+    cursor: "not-allowed",
+  };
   const [currentPage, setCurrentPage] = useState(0);
   const [familyHistory, setFamilyHistory] = useState([]);
   const [socialHistory, setSocialHistory] = useState({});
   const [currentFamily, setCurrentFamily] = useState({});
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -73,13 +80,18 @@ export default function SocialHistory() {
 
   const handleVisitClick = (i) => {
     console.log(familyHistory[i]);
-    // Increment the currentPage when the user clicks the div
     setCurrentPage(currentPage + 1);
+  };
+
+  const handleSocialHistoryChange = (e, index) => {
+    const newSocialHistory = [...sHistory];
+    newSocialHistory[index].value = e.target.value;
+    setSHistory(newSocialHistory);
   };
 
   return (
     <>
-      {currentPage === 0 ? (
+      {currentPage === 0 && (
         <>
           <div className="text-black text-base font-bold leading-5 mt-8 mb-5 max-md:ml-1 max-md:mt-10 flex justify-between items-center">
             FAMILY HISTORY
@@ -110,18 +122,37 @@ export default function SocialHistory() {
             </button>
           ))}
 
-<div className="text-black text-base font-bold leading-5 mt-8 mb-5 max-md:ml-1 max-md:mt-10 flex justify-between items-center">
+          <div className="text-black text-base font-bold leading-5 mt-8 mb-5 max-md:ml-1 max-md:mt-10 flex justify-between items-center">
             SOCIAL HISTORY
-            <button
-              className="flex gap-1.5 justify-between px-10 py-1 rounded border-blue-800 text-blue-800 border-solid text-xs font-semibold border-1.5"
-              onClick={() => setCurrentPage(3)}
-            >
-              Edit Social History
-            </button>
+            <div>
+              {!isEditingSocialHistory && (
+                <button
+                  className="flex gap-1.5 justify-between px-10 py-1 rounded border-blue-800 text-blue-800 border-solid text-xs font-semibold border-1.5"
+                  onClick={() => {
+                    setOriginalSocialHistory(socialHistory); // Store the original data
+                    setIsEditingSocialHistory(true);
+                  }}
+                >
+                  Edit Social History
+                </button>
+              )}
+              {isEditingSocialHistory && (
+                <button
+                  className="flex gap-1.5 justify-between px-10 py-1 rounded border-blue-800 text-blue-800 border-solid text-xs font-semibold border-1.5"
+                  onClick={() => {
+                    setSocialHistory(originalSocialHistory); // Revert to original data
+                    setIsEditingSocialHistory(false);
+                  }}
+                >
+                  Cancel
+                </button>
+              )}
+            </div>
           </div>
 
           <table className="max-w-fit border-spacing-y-7 border-separate">
-            {sHistory.map((item) => (
+            {" "}
+            {sHistory.map((item, index) => (
               <tr key={item.variable}>
                 <td className="w-5">
                   <Image
@@ -140,38 +171,87 @@ export default function SocialHistory() {
                 </td>
                 <td className="border-l-[5rem] border-transparent">
                   <div className="text-black text-xs leading-5 ml-auto">
-                    {item.value}
+                    {isEditingSocialHistory ? (
+                      item.variable === "Smoking Status" ? (
+                        <select
+                          onChange={(e) => handleSocialHistoryChange(e, index)}
+                          value={item.value}
+                          className="text-black text-sm whitespace-nowrap rounded shadow-sm flex-shrink-0 justify-center items-stretch mt-[10px] px-2 py-2.5 border-[0.5px] border-solid border-black"
+                        >
+                          <option value="">Select</option>
+                          <option value="Smoker">Smoker</option>
+                          <option value="Non-Smoker">Not a Smoker</option>
+                        </select>
+                      ) : item.variable === "Cigarettes Per Day" ? (
+                        <input
+                          type="number"
+                          value={item.value}
+                          onChange={(e) => handleSocialHistoryChange(e, index)}
+                          className="text-black text-sm whitespace-nowrap rounded shadow-sm flex-shrink-0 justify-center items-stretch mt-[10px] px-2 py-2.5 border-[0.5px] border-solid border-black"
+                          disabled={sHistory[0].value === "Non-Smoker"}
+                          style={
+                            sHistory[0].value === "Non-Smoker"
+                              ? disabledInputStyle
+                              : {}
+                          }
+                        />
+                      ) : item.variable === "Physical Activities" ? (
+                        <select
+                          onChange={(e) => handleSocialHistoryChange(e, index)}
+                          value={item.value}
+                          className="text-black text-sm whitespace-nowrap rounded shadow-sm flex-shrink-0 justify-center items-stretch mt-[10px] px-2 py-2.5 border-[0.5px] border-solid border-black"
+                        >
+                          <option value="">Select</option>
+                          <option value="Sedentary">Sedentary</option>
+                          <option value="Low">Low Activity</option>
+                          <option value="ModerateAct">Moderate Activity</option>
+                          <option value="High">High Activity</option>
+                          <option value="Regular">Regular Exercise</option>
+                        </select>
+                      ) : item.variable === "Alcohol" ? (
+                        <select
+                          onChange={(e) => handleSocialHistoryChange(e, index)}
+                          value={item.value}
+                          className="text-black text-sm whitespace-nowrap rounded shadow-sm flex-shrink-0 justify-center items-stretch mt-[10px] px-2 py-2.5 border-[0.5px] border-solid border-black"
+                        >
+                          <option value="">Select</option>
+                          <option value="Non-Drinker">Non-Drinker</option>
+                          <option value="Moderate">Moderate Drinker</option>
+                          <option value="Heavy">Heavy Drinker</option>
+                        </select>
+                      ) : (
+                        <input
+                          type="text"
+                          value={item.value}
+                          onChange={(e) => handleSocialHistoryChange(e, index)}
+                          className="text-black text-sm whitespace-nowrap rounded shadow-sm flex-shrink-0 justify-center items-stretch mt-[10px] px-2 py-2.5 border-[0.5px] border-solid border-black"
+                        />
+                      )
+                    ) : (
+                      item.value
+                    )}
                   </div>
                 </td>
               </tr>
             ))}
           </table>
+          {isEditingSocialHistory && (
+            <button
+              className="self-end gap-1.5 justify-between px-10 py-1 rounded border-sky-900  bg-sky-900 text-white border-solid text-xs font-semibold border-1.5"
+              onClick={() => {
+                // Logic for saving changes
+                setIsEditingSocialHistory(false);
+              }}
+            >
+              Save
+            </button>
+          )}
         </>
-      ) : (
-        ""
       )}
 
-      {currentPage === 1 ? (
-        <>
-          <FamilyHistory data={currentFamily} />
-        </>
-      ) : (
-        ""
-      )}
-       {currentPage === 2 ? (
-        <>
-          <EditFamilyHistory />
-        </>
-      ) : (
-        ""
-      )}
-       {currentPage === 3 ? (
-        <>
-          <EditSocialHistory />
-        </>
-      ) : (
-        ""
-      )}
+      {currentPage === 1 && <FamilyHistory data={currentFamily} />}
+
+      {currentPage === 2 && <EditFamilyHistory />}
     </>
   );
 }
