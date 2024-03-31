8d1e769b634c329a21d4938cd010d4abc67f13dd
Andrei <misael_marin@dlsu.edu.ph>
2024-03-31 15:19:50 +0800

    providersupdate

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -12,47 +12,48 @@ export function Providers({ children }: { children: React.ReactNode }) {
 	const router = useRouter();
 	const pathname = usePathname();
 	const current = currentUser();
-	// useEffect(() => {
-	// 	const getAuth = async () => {
-	// 		const authData = await authentication.getSession();
-	// 		if (authData instanceof AuthError) {
-	// 			console.error(authData.message);
-	// 			return;
-	// 		}
-	// 		if (!authData?.session?.user.id) {
-	// 			if (
-	// 				pathname !== "/" &&
-	// 				pathname !== "/login" &&
-	// 				pathname !== "/test" &&
-	// 				pathname !== "/patient_form" &&
-	// 				pathname !== "/doctor_form" &&
-	// 				pathname !== "/require_auth" &&
-	// 				!pathname.includes("/legal")
-	// 			) {
-	// 				router.push("/require_auth");
-	// 			}
-	// 		} else {
-	// 			if (
-	// 				pathname === "/" ||
-	// 				pathname === "/login" ||
-	// 				pathname === "/test" ||
-	// 				pathname === "/patient_form" ||
-	// 				pathname === "/doctor_form"
-	// 			) {
-	// 				if (current.user.type === "patient") {
-	// 					router.push("/patient/home");
-	// 				} else if (current.user.type === "doctor") {
-	// 					if (current.user.specialization_id === 1) {
-	// 						router.push("/home");
-	// 					} else {
-	// 						router.push("/other_doctor/referrals");
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	};
+	useEffect(() => {
+		const getAuth = async () => {
+			const authData = await authentication.getSession();
+			if (authData instanceof AuthError) {
+				console.error(authData.message);
+				return;
+			}
+			if (!authData?.session?.user.id) {
+				if (
+					pathname !== "/" &&
+					pathname !== "/login" &&
+					pathname !== "/test" &&
+					pathname !== "/patient_form" &&
+					pathname !== "/doctor_form" &&
+					pathname !== "/require_auth" &&
+					!pathname.includes("/legal")&&
+					!pathname.includes("/middleware")
+				) {
+					router.push("/require_auth");
+				}
+			} else {
+				if (
+					pathname === "/" ||
+					pathname === "/login" ||
+					pathname === "/test" ||
+					pathname === "/patient_form" ||
+					pathname === "/doctor_form"
+				) {
+					if (current.user.type === "patient") {
+						router.push("/patient/home");
+					} else if (current.user.type === "doctor") {
+						if (current.user.specialization_id === 1) {
+							router.push("/home");
+						} else {
+							router.push("/other_doctor/referrals");
+						}
+					}
+				}
+			}
+		};
 
-	// 	getAuth();
-	// }, [router, pathname, current.user]);
+		getAuth();
+	}, [router, pathname, current.user]);
 	return <NextUIProvider>{children}</NextUIProvider>;
 }
