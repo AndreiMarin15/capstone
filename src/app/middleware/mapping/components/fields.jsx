f19540a4ed70d3ca99f1e6b45f6ae787c6fb124b
Andrei <misael_marin@dlsu.edu.ph>
2024-04-09 01:12:34 +0800

    update

--- src/app/middleware/mapping/components/fields.jsx
+++ src/app/middleware/mapping/components/fields.jsx
@@ -20,7 +20,7 @@ export default function Fields({ fields, onHoverOver, onHover, hoveredImageId, t
 		const mapping_data = {
 			api_key: apiKey,
 			owner_email: middlewareUser.getState().info.email,
-			// table_mapping: { table_name: table, [table]: clientTable },
+			table_mapping: { table_name: table, [table]: clientTable },
 			column_mapping: toSave,
 			client_table: clientTable,
 			fhir_table: table,
