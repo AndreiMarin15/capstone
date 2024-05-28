c48386655bfb73c5ce78dc489d3e929591c55c9a
nicoletabije <nicole_tabije@dlsu.edu.ph>
2024-05-28 13:55:45 +0800

    fixed records PDFs

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -3,21 +3,20 @@
 import { CarePlansPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/careplanlist";
 import { FamilySocialHistoryPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/familysocialhistoryPDF";
 import { LabTestHistoryPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/labtesthistoryPDF";
-import { MasterDataPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/masterdatapdf";
+import { MasterDataPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/masterdataPDF";
 import { MedicalHistoryPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/medicalhistoryPDF";
-import { MedicationHistoryPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/medicationhistory";
+import { MedicationHistoryPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/medicationhistoryPDF";
 import { ReferralHistoryPDF } from "../health_records/about_patient/[patient_id]/components/PDF_templates/referralhistoryPDF";
 export default function Test() {
-	return (
-		<div>
-			<CarePlansPDF />
-			<FamilySocialHistoryPDF />
-			<LabTestHistoryPDF />
-			<MasterDataPDF />
-			<MedicalHistoryPDF />
-			<MedicationHistoryPDF />
-			<ReferralHistoryPDF />
-			
-		</div>
-	);
+  return (
+    <div>
+      <CarePlansPDF />
+      <FamilySocialHistoryPDF />
+      <LabTestHistoryPDF />
+      <MasterDataPDF />
+      <MedicalHistoryPDF />
+      <MedicationHistoryPDF />
+      <ReferralHistoryPDF />
+    </div>
+  );
 }
