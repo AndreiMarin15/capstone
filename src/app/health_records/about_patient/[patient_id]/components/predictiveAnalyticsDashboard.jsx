c67a67aa5febdc7c137506d9e0b93188bed01bc9
nicoletabije <nicole_tabije@dlsu.edu.ph>
2024-05-29 12:10:09 +0800

    added UI

--- src/app/health_records/about_patient/[patient_id]/components/predictiveAnalyticsDashboard.jsx
+++ src/app/health_records/about_patient/[patient_id]/components/predictiveAnalyticsDashboard.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import axios from "axios";
 import * as React from "react";
+import { Button } from "@/components/ui/button";
 export default function PredictiveAnalytics(patientId) {
   // PUBLIC
   // patient:
@@ -33,19 +34,19 @@ export default function PredictiveAnalytics(patientId) {
   const wordMatch = {
     male: "Gender",
     age: "Age",
-    // education: 4,
+    education: "Education",
     currentSmoker: "Currently a Smoker",
     cigsPerDay: "Cigarettes per Day",
     BPMeds: "Taking Blood Pressure Medications",
     prevalentStroke: "Prevalent Strokes in the Past",
     prevalentHyp: "Prevalent Hypertensions in the Past",
-    diabetes: 0,
-    totChol: 238,
-    sysBP: 125,
-    diaBP: 80,
-    BMI: 19.36,
-    heartRate: 60,
-    glucose: 66,
+    diabetes: "Has Diabetes",
+    totChol: "Total Cholesterol",
+    sysBP: "Systolic",
+    diaBP: "Diastolic",
+    BMI: "BMI",
+    heartRate: "Heart Rate",
+    glucose: "Glucose",
   };
   const pAnalytics = {
     male: 1,
@@ -64,7 +65,28 @@ export default function PredictiveAnalytics(patientId) {
     heartRate: 60,
     glucose: 66,
   };
-
+  const formatValue = (key, value) => {
+    const boolean = [
+      "currentSmoker",
+      "BPMeds",
+      "prevalentStroke",
+      "prevalentHyp",
+      "diabetes",
+    ];
+    const bp = ["sysBP", "diaBP"];
+    if (key == "male") {
+      if (value == 1) return "Male";
+      return "Female";
+    }
+    if (boolean.includes(key)) {
+      if (value == 1) return "Yes";
+      return "No";
+    }
+    if (bp.includes(key)) return value + " mmHg";
+    if (key == "totChol") return value + " mm/dL";
+    if (key == "glucose") return value + " mg/dL";
+    return value;
+  };
   const [percentage, setPercentage] = React.useState(0);
 
   React.useEffect(() => {
@@ -82,59 +104,25 @@ export default function PredictiveAnalytics(patientId) {
       <div className="text-black text-xs font-bold leading-5 mt-8 mb-1 max-md:ml-1 max-md:mt-10">
         PREDICTIVE ANALYTICS
       </div>
-
-      <table className="max-w-fit border-spacing-y-7 border-separate text-black">
-        {(() => {
-          const rows = [];
-          for (let i = 0; i < pAnalytics.length; i += 2) {
-            const item1 = pAnalytics[i];
-            const item2 = i + 1 < pAnalytics.length ? pAnalytics[i + 1] : null;
-
-            rows.push(
-              <tr key={`${item1.variable}-${item2 ? item2.variable : ""}`}>
-                <td className="border-l-[16px] border-transparent">
-                  <div className="text-black text-xs font-semibold leading-5 self-center my-auto text-left">
-                    {item1.variable}
-                  </div>
-                </td>
-
-                <td className="border-l-[5rem] border-transparent text-center">
-                  <div className="text-black text-xs leading-5 ml-auto text-left">
-                    {item1.value}
-                  </div>
-                </td>
-
-                {item2 && (
-                  <td className="border-l-[10rem] border-transparent">
-                    <div className="text-black text-xs font-semibold leading-5 self-center my-auto text-left">
-                      {item2.variable}
-                    </div>
-                  </td>
-                )}
-
-                {item2 && (
-                  <td className="border-l-[10rem] border-transparent text-center">
-                    <div className="text-black text-xs leading-5 ml-auto text-left">
-                      {item2.value}
-                    </div>
-                  </td>
-                )}
-              </tr>
-            );
-          }
-          return rows;
-        })()}
+      <table className="max-w-fit border-spacing-y-5 border-spacing-x-[5em] border-separate text-xs">
+        {Object.keys(pAnalytics).map((key, i) => (
+          <>
+            <tr key={i}>
+              <td>{wordMatch[key]}</td>
+              <td>{formatValue(key, pAnalytics[key])}</td>
+            </tr>
+          </>
+        ))}
       </table>
 
       <div className="self-center flex aspect-[3.3333333333333335] flex-col justify-center items-stretch my-auto">
-        <button
+        <Button
           onClick={() => {
             window.location.href = "/health_records/about_patient";
           }}
-          className="text-white text-xs font-semibold leading-5 whitespace-nowrap bg-sky-900 aspect-[3.3333333333333335] mt-10 justify-center items-stretch px-5 py-1.5 rounded max-md:px-5"
         >
           Generate
-        </button>
+        </Button>
       </div>
     </>
   );
