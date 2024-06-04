3d281adccfccdaa353e5282a88ff8ebd512f9ac4
yanajuson <kay_eliana_juson@dlsu.edu.ph>
2024-06-04 13:10:47 +0800

    Lab Tests Patient-end (under letters) DONE

--- src/app/health_records/about_patient/[patient_id]/components/labTestsDashboard.jsx
+++ src/app/health_records/about_patient/[patient_id]/components/labTestsDashboard.jsx
@@ -82,148 +82,140 @@ export default function LabTests({ patientId }) {
 
   return (
     <>
-      {currentScreen === 0 && (
-        <div>
-          <div className="flex justify-between">
-            <div className="text-black text-base font-bold leading-5 mt-8 mb-1 max-md:ml-1 max-md:mt-10">
-              LAB TESTS
-            </div>
-            <div className="flex items-center">
-              {/* <span className="flex items-center gap-1 px-1 py-1 mt-8 rounded-md border-[0.5px] border-solid border-black font-normal mr-2">
-								<Image
-									alt="picture"
-									height={0}
-									width={0}
-									loading="lazy"
-									src="https://cdn.builder.io/api/v1/image/assets/TEMP/872489d37c6f07090c71fb194a8c077334f5ee8d7e865b4e470f49f5a27b95ba?apiKey=66e07193974a40e683930e95115a1cfd&"
-									className="aspect-[0.86] object-contain object-center w-3 overflow-hidden"
-								/>
-								<div className="text-black text-xs leading-5 self-center whitespace-nowrap">FILTER</div>
-							</span>
-							<span className="flex items-center gap-1 px-1 py-1 mt-8 rounded-md border-[0.5px] border-solid border-black font-normal">
-								<Image
-									alt="picture"
-									height={0}
-									width={0}
-									loading="lazy"
-									src="https://cdn.builder.io/api/v1/image/assets/TEMP/49eeb01b15c87289299d3123ede7ccfbf333d278cb9ddfc7f5674a94c5d52e26?apiKey=66e07193974a40e683930e95115a1cfd&"
-									className="aspect-[0.86] object-contain object-center w-3 overflow-hidden"
-								/>
-								<div className="text-black text-xs leading-5 self-center">SORT</div>
-							</span> */}
-              <span className="flex items-center gap-1 px-1 py-1 mt-8 rounded-md font-normal mr-2">
-                <Button variant="outline">Self-Pricking</Button>
-              </span>
-            </div>
-          </div>
-          <Tabs defaultValue="all" className="w-[400px]">
-            <TabsList>
-              <TabsTrigger value="all">All</TabsTrigger>
-              <TabsTrigger value="endocrinologist">Endocrinologist</TabsTrigger>
-              <TabsTrigger value="cardiologist">Cardiologist</TabsTrigger>
-              <TabsTrigger value="gastroenterologist">
-                Gastroenterologist
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="account">{/* Add contents here */}</TabsContent>
-            <TabsContent value="endocrinologist">
-              {/* Add contents here */}
-            </TabsContent>
-            <TabsContent value="cardiologist">
-              {/* Add contents here */}
-            </TabsContent>
-            <TabsContent value="gastroenterologist">
-              {/* Add contents here */}
-            </TabsContent>
-          </Tabs>
-
-          {labTests?.map((labTest, index) => (
-            <button
-              key={index}
-              className="flex flex-col mt-10 items-start text-xs leading-5 text-black max-w-[650px]"
-              onClick={() => {
-                if (labTest.status !== "requested") {
-                  handleMedicationClick(labTest);
-                  setSelectedObservationId(labTest.id);
-                }
-              }}
-              disabled={labTest.status === "requested"}
-            >
-              <div className="flex gap-3.5 font-semibold whitespace-nowrap">
-                <Image
-                  alt="image"
-                  height={0}
-                  width={0}
-                  loading="lazy"
-                  src={labTest.src}
-                  className="aspect-square fill-black w-[15px]"
-                />
-                {/* <div className="my-auto">{labTest.variable}</div> */}
-                <div className="my-auto">Lab Test Request #1</div>
+      {currentScreen === 0 ? (
+        <>
+          <div>
+            <div className="flex justify-between">
+              <div className="text-black text-base font-bold leading-5 mt-8 mb-1 max-md:ml-1 max-md:mt-10">
+                LAB TESTS
               </div>
-              <div className="flex gap-5 justify-between ml-7 max-md:ml-2.5 w-[100%]">
-                <div className="flex gap-1 justify-between font-medium whitespace-nowrap">
+              <div className="flex items-center">
+                {/* <span className="flex items-center gap-1 px-1 py-1 mt-8 rounded-md border-[0.5px] border-solid border-black font-normal mr-2">
+        <Image
+         alt="picture"
+         height={0}
+         width={0}
+         loading="lazy"
+         src="https://cdn.builder.io/api/v1/image/assets/TEMP/872489d37c6f07090c71fb194a8c077334f5ee8d7e865b4e470f49f5a27b95ba?apiKey=66e07193974a40e683930e95115a1cfd&"
+         className="aspect-[0.86] object-contain object-center w-3 overflow-hidden"
+        />
+        <div className="text-black text-xs leading-5 self-center whitespace-nowrap">FILTER</div>
+       </span>
+       <span className="flex items-center gap-1 px-1 py-1 mt-8 rounded-md border-[0.5px] border-solid border-black font-normal">
+        <Image
+         alt="picture"
+         height={0}
+         width={0}
+         loading="lazy"
+         src="https://cdn.builder.io/api/v1/image/assets/TEMP/49eeb01b15c87289299d3123ede7ccfbf333d278cb9ddfc7f5674a94c5d52e26?apiKey=66e07193974a40e683930e95115a1cfd&"
+         className="aspect-[0.86] object-contain object-center w-3 overflow-hidden"
+        />
+        <div className="text-black text-xs leading-5 self-center">SORT</div>
+       </span> */}
+                <span className="flex items-center gap-1 px-1 py-1 mt-8 rounded-md font-normal mr-2">
+                  <Button
+                    variant="outline"
+                    onClick={() => {
+                      setCurrentScreen(2);
+                    }}
+                  >
+                    Self-Pricking
+                  </Button>
+                </span>
+              </div>
+            </div>
+
+            {labTests?.map((labTest, index) => (
+              <button
+                key={index}
+                className="flex flex-col mt-10 items-start text-xs leading-5 text-black max-w-[650px]"
+                onClick={() => {
+                  if (labTest.status !== "requested") {
+                    handleMedicationClick(labTest);
+                    setSelectedObservationId(labTest.id);
+                  }
+                }}
+                disabled={labTest.status === "requested"}
+              >
+                <div className="flex gap-3.5 font-semibold whitespace-nowrap">
                   <Image
                     alt="image"
                     height={0}
                     width={0}
                     loading="lazy"
-                    src={labTest.srcdoctor}
+                    src={labTest.src}
                     className="aspect-square fill-black w-[15px]"
                   />
-                  <div className="grow my-auto">{labTest.doctor}</div>
+                  {/* <div className="my-auto">{labTest.variable}</div> */}
+                  <div className="my-auto">Lab Test Request #1</div>
                 </div>
-                <div
-                  className="flex-auto my-auto"
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <div>
-                    <span style={{ fontWeight: "bold" }}>Date Requested:</span>{" "}
-                    {labTest.reqdate}
+                <div className="flex gap-5 justify-between ml-7 max-md:ml-2.5 w-[100%]">
+                  <div className="flex gap-1 justify-between font-medium whitespace-nowrap">
+                    <Image
+                      alt="image"
+                      height={0}
+                      width={0}
+                      loading="lazy"
+                      src={labTest.srcdoctor}
+                      className="aspect-square fill-black w-[15px]"
+                    />
+                    <div className="grow my-auto">
+                      {labTest.doctor} Dr. Harold Chiu
+                    </div>
                   </div>
-                  {labTest.status === "final" && (
+                  <div
+                    className="flex-auto my-auto"
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <div>
+                      <span style={{ fontWeight: "bold" }}>
+                        Date Requested:
+                      </span>{" "}
+                      {labTest.reqdate}
+                    </div>
+                    {/* {labTest.status === "final" && (
                     <div style={{ marginLeft: "8px" }}>
                       <span style={{ fontWeight: "bold" }}>Date Uploaded:</span>{" "}
                       {labTest.update}
                     </div>
+                  )} */}
+                  </div>
+                  {labTest.status === "requested" && (
+                    <div className="text-black text-xs font-medium leading-5 flex items-center">
+                      <svg
+                        className="h-3 w-3 ml-1 text-red-500"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <circle cx="10" cy="10" r="5" />
+                      </svg>
+                      Requested
+                    </div>
                   )}
-                </div>
-                {labTest.status === "requested" && (
-                  <div className="text-black text-xs font-medium leading-5 flex items-center">
-                    <svg
-                      className="h-3 w-3 ml-1 text-red-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="10" cy="10" r="5" />
-                    </svg>
-                    Requested
+                  {labTest.status === "final" && (
+                    <div className="text-black text-xs font-medium leading-5 flex items-center">
+                      <svg
+                        className="h-3 w-3 ml-1 text-green-500"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <circle cx="10" cy="10" r="5" />
+                      </svg>
+                      Uploaded
+                    </div>
+                  )}
+                  <div className="text-xs text-blue-500 leading-5 flex ml-5 items-center">
+                    <Button variant="download"> ↓ Download (.pdf)</Button>
                   </div>
-                )}
-                {labTest.status === "final" && (
-                  <div className="text-black text-xs font-medium leading-5 flex items-center">
-                    <svg
-                      className="h-3 w-3 ml-1 text-green-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="10" cy="10" r="5" />
-                    </svg>
-                    Uploaded
-                  </div>
-                )}
-                <div className="text-xs text-blue-500 leading-5 flex ml-5 items-center">
-                  <button>Download (.pdf)</button>
                 </div>
-              </div>
-            </button>
-          ))}
-          <BackButton />
-        </div>
-      )}
-      {currentScreen === 1 && (
+              </button>
+            ))}
+            <BackButton />
+          </div>
+        </>
+      ) : currentScreen === 1 ? (
         // <VisitLabTests
         //   currentScreen={currentScreen}
         //   setCurrentScreen={setCurrentScreen}
@@ -234,6 +226,15 @@ export default function LabTests({ patientId }) {
           setCurrentScreen={setCurrentScreen}
           observationId={selectedObservationId}
         />
+      ) : currentScreen === 2 ? (
+        /* CHANGE TO SELF PRICK JSX HERE */
+        <ViewLabRequest
+          currentScreen={currentScreen}
+          setCurrentScreen={setCurrentScreen}
+          observationId={selectedObservationId}
+        />
+      ) : (
+        ""
       )}
     </>
   );
---
+++ src/app/patient/letters/components/labTests.jsx
@@ -0,0 +1,154 @@
+"use client";
+import * as React from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
+import ViewLab from "./sub_components/viewLab";
+import SelfPrick from "./sub_components/addPrick";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
+} from "@/components/ui/dropdown-menu";
+
+export default function LabTests() {
+  const router = useRouter();
+
+  const [currentScreen, setCurrentScreen] = useState(0);
+
+  const handleButtonClick = () => {
+    // Set currentScreen to the desired value when a medication item is clicked
+    setCurrentScreen(1); // Assuming the desired value for the second screen is 1
+    console.log("current Screen:", currentScreen);
+  };
+
+  return (
+    <>
+      {currentScreen === 0 ? (
+        <TabsContent value="labtestrequest">
+          <div className="flex justify-between items-center mt-4">
+            <div className="font-semibold items-center self-center text-s ml-5">
+              Lab Test Requests
+            </div>
+            <div className="flex gap-2">
+              <div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <button className="grow justify-center text-xs px-6 py-2 rounded-md border border-black border-solid">
+                      SORT
+                    </button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Sort By Doctor Name</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuRadioGroup>
+                      <DropdownMenuRadioItem value="asc">
+                        A-Z
+                      </DropdownMenuRadioItem>
+                      <DropdownMenuRadioItem value="desc">
+                        Z-A
+                      </DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              <div className="">
+                <Button
+                  variant="outline"
+                  onClick={() => {
+                    setCurrentScreen(2);
+                  }}
+                >
+                  Self-Pricking
+                </Button>
+              </div>
+            </div>
+          </div>
+          <button
+            className="flex flex-col mt-5 items-start text-xs leading-5 text-black max-w-[650px]"
+            onClick={handleButtonClick}
+          >
+            <div className="ml-5 flex gap-2 items-center">
+              <Image
+                alt="image"
+                height={0}
+                width={0}
+                loading="lazy"
+                src={
+                  "https://cdn.builder.io/api/v1/image/assets/TEMP/4a525f62acf85c2276bfc82251c6beb10b3d621caba2c7e3f2a4701177ce98c2?"
+                }
+                className="aspect-square fill-black w-[10px]"
+              />
+              {/* Name of Medicine */}
+              <div className="text-xs font-semibold">
+                {/*  {medication.resource.medicationCodeableConcept[0].text} */}
+                Lab Test Request #1
+              </div>
+            </div>
+            <div className="flex gap-5 justify-between ml-7  max-md:ml-2.5 w-[100%]">
+              <div className="flex gap-1 justify-between font-medium whitespace-nowrap">
+                <Image
+                  alt="image"
+                  height={0}
+                  width={0}
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/cafd760f8d1e87590398c40d6e223fabf124ae3120c9f867d6b2fc048ac936ec?"
+                  className="aspect-square fill-black w-[15px]"
+                />
+                <div className="grow my-auto">Dr. Harold Chiu</div>
+              </div>
+              <div
+                className="flex-auto my-auto"
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div>
+                  <span className="font-semibold">Date Requested:</span>{" "}
+                  2024/01/24
+                </div>
+              </div>
+
+              <div className="text-black text-xs font-medium leading-5 flex items-center">
+                <svg
+                  className="h-3 w-3 ml-1 text-red-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="10" cy="10" r="5" />
+                </svg>
+                Requested
+              </div>
+
+              <div className="text-xs text-blue-500 leading-5 flex ml-5 items-center">
+                <Button variant="download"> ↓ Download (.pdf)</Button>
+              </div>
+            </div>
+          </button>
+        </TabsContent>
+      ) : currentScreen === 1 ? (
+        <ViewLab
+          currentScreen={currentScreen}
+          setCurrentScreen={setCurrentScreen}
+        />
+      ) : currentScreen === 2 ? (
+        /* CHANGE TO SELF PRICK JSX HERE */
+        <SelfPrick
+          currentScreen={currentScreen}
+          setCurrentScreen={setCurrentScreen}
+        />
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
