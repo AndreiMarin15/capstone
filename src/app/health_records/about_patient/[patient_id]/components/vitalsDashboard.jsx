6a5eed004ae36c72a53a22b89175acca2476ad90
Lastronus <kenji.hartigan.go@gmail.com>
2024-06-12 22:40:10 +0800

    Biometric graphs

--- src/app/health_records/about_patient/[patient_id]/components/vitalsDashboard.jsx
+++ src/app/health_records/about_patient/[patient_id]/components/vitalsDashboard.jsx
@@ -454,6 +454,7 @@ export default function Vitals({ patientId }) {
 					setCurrentPage={setCurrentPage}
 					defaultMetric={selectedMetric}
 					chartValues={chartValues}
+					renderingOptions={renderingOptions}
 				/>
 			)}
 		</>
