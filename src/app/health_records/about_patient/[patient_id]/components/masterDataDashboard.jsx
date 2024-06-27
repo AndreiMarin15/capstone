07132dc7032ecc0b486f90f47bdd52c18646ea62
nicoletabije <nicole_tabije@dlsu.edu.ph>
2024-06-27 11:22:17 +0800

    careplan collab doctors & education

--- src/app/health_records/about_patient/[patient_id]/components/masterDataDashboard.jsx
+++ src/app/health_records/about_patient/[patient_id]/components/masterDataDashboard.jsx
@@ -8,7 +8,24 @@ import AddAttendingDoctors from "./sub_components/addAttendingDoctors";
 import { Button } from "@/components/ui/button";
 export default function MasterData({ patientId }) {
   const [currentScreen, setCurrentScreen] = useState(0);
+  const [showModal, setShowModal] = useState(false); // modal visibility
+  const [status, setStatus] = useState("Living"); // modal for patient status
+
+  const handleViewClick = () => {
+    setStatus("Living"); //  living as initial
+    setShowModal(true); //
+  };
+
   const [mData, setmData] = useState([
+    {
+      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/1af603f1f29245d416cded12308f14442a26e02b25f3257fb76c448339e0e368?",
+      variable: "Status",
+      value: (
+        <Button variant="outline" onClick={handleViewClick}>
+          {status}
+        </Button>
+      ),
+    },
     {
       src: "https://cdn.builder.io/api/v1/image/assets/TEMP/86bc0813aecf897cafa42df901705c229a0a744cbf822394277aece4f7f5aa61?",
       variable: "Name",
@@ -27,6 +44,11 @@ export default function MasterData({ patientId }) {
     {
       src: "https://cdn.builder.io/api/v1/image/assets/TEMP/4c3ec2f045c5a91d05c1f074f660097897b8fc83403da81ed7f44111303ef22f?",
       variable: "Gender",
+      value: "",
+    },
+    {
+      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/23f789239697f8a78777e02109dfca5430a50c87ad9728340db8e9d8b061ab26?",
+      variable: "Education",
       value: "",
     },
     {
@@ -169,6 +191,67 @@ export default function MasterData({ patientId }) {
           />
         </>
       ) : null}
+      {showModal && (
+        <div
+          className="fixed z-10 inset-0 overflow-y-auto"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+              aria-hidden="true"
+            ></div>
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-title"
+                >
+                  Set the status of the patient
+                </h3>
+                <div className="mt-2">
+                  <input
+                    type="radio"
+                    id="living"
+                    name="status"
+                    value="Living"
+                    checked={status === "Living"}
+                    onChange={(e) => setStatus(e.target.value)}
+                  />
+                  <label htmlFor="living">Living</label>
+                  <br />
+                  <input
+                    type="radio"
+                    id="deceased"
+                    name="status"
+                    value="Deceased"
+                    checked={status === "Deceased"}
+                    onChange={(e) => setStatus(e.target.value)}
+                  />
+                  <label htmlFor="deceased">Deceased</label>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Confirm
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- src/backend/attending_doctors/attending_doctors.js
+++ src/backend/attending_doctors/attending_doctors.js
@@ -4,50 +4,60 @@ import { sendNotification } from "../sendNotification.js";
 const project = client("project");
 
 export const addAttendingDoctor = async (doctor, patient) => {
-	const attendingDoctor = {
-		patient_id: patient.id,
-		doctor_id: doctor.id,
-		license_id: doctor.license_id,
-		doctor_first_name: doctor.first_name,
-		doctor_last_name: doctor.last_name,
-		doctor_specialization: doctor.doctor_specialization,
-		doctor_years: doctor.years,
-		status: doctor.status ?? "pending",
-		clinic: doctor.clinic,
-		contact: doctor.contact,
-	};
-	console.log(attendingDoctor);
-	const { data, error } = await project.from("attending_doctors").insert([attendingDoctor]);
-	console.log(data);
-	console.log(error);
-	if (error) {
-		throw error;
-	}
+  const attendingDoctor = {
+    patient_id: patient.id,
+    doctor_id: doctor.id,
+    license_id: doctor.license_id,
+    doctor_first_name: doctor.first_name,
+    doctor_last_name: doctor.last_name,
+    doctor_specialization: doctor.doctor_specialization,
+    doctor_years: doctor.years,
+    status: doctor.status ?? "pending",
+    clinic: doctor.clinic,
+    contact: doctor.contact,
+  };
+  console.log(attendingDoctor);
+  const { data, error } = await project
+    .from("attending_doctors")
+    .insert([attendingDoctor]);
+  console.log(data);
+  console.log(error);
+  if (error) {
+    throw error;
+  }
 
-	await sendNotification(patient.id, "New Referral", "You have a new referral", doctor.id);
+  await sendNotification(
+    patient.id,
+    "New Referral",
+    "You have a new referral",
+    doctor.id
+  );
 
-	return data;
+  return data;
 };
 
 export const getAttendingDoctors = async (patient) => {
-	const { data, error } = await project
-		.from("attending_doctors")
-		.select("*")
-		.eq("patient_id", patient ?? currentUser.getState().user.id);
+  const { data, error } = await project
+    .from("attending_doctors")
+    .select("*")
+    .eq("patient_id", patient ?? currentUser.getState().user.id);
 
-	if (error) {
-		throw error;
-	}
+  if (error) {
+    throw error;
+  }
 
-	return data;
+  return data;
 };
 
 export const deleteAttendingDoctor = async (id) => {
-	const { data, error } = await project.from("attending_doctors").delete().eq("id", id);
+  const { data, error } = await project
+    .from("attending_doctors")
+    .delete()
+    .eq("id", id);
 
-	if (error) {
-		throw error;
-	}
+  if (error) {
+    throw error;
+  }
 
-	return data;
+  return data;
 };
